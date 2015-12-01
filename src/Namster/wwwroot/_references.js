2632b80302047c1a87cc99bb35c745d39b415f28
John Knoll <jpknoll@ucdavis.edu>
2015-12-01 15:45:53 -0800

    stuff

--- src/Namster/wwwroot/_references.js
+++ src/Namster/wwwroot/_references.js
@@ -1,6 +1,5 @@
 ﻿/// <autosync enabled="true" />
 /// <reference path="../gulpfile.js" />
-/// <reference path="../site.js" />
 /// <reference path="js/site.js" />
 /// <reference path="lib/bootstrap/dist/js/bootstrap.js" />
 /// <reference path="lib/bootstrap-touch-carousel/dist/js/bootstrap-touch-carousel.js" />
