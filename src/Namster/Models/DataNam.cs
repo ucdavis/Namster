de080ba50b5a2e0c271a635913062ab31b86f608
John Knoll <jpknoll@ucdavis.edu>
2015-12-02 11:09:36 -0800

    invalid characters

--- src/Namster/Models/DataNam.cs
+++ src/Namster/Models/DataNam.cs
@@ -10,28 +10,27 @@ namespace Namster.Models
     {
         [ElasticProperty(Index = FieldIndexOption.NotAnalyzed)]
         public string NamNumber { get; set; }
-​
+
         public string Building { get; set; }
-​
+
         public string Room { get; set; }
+
         public string Department { get; set; }
-​
+
         [ElasticProperty(Index = FieldIndexOption.No, Store = true)]
         public string CaanZone { get; set; }
-​
+
         [ElasticProperty(Index = FieldIndexOption.No, Store = true)]
         public string BillingId { get; set; }
-​
+
         [ElasticProperty(Index = FieldIndexOption.NotAnalyzed)]
         public string Vlan { get; set; }
-​
+
         [ElasticProperty(Index = FieldIndexOption.No, Store = true)]
         public string DepartmentNumber { get; set; }
-​
+
         public string Division { get; set; }
-​
+
         public string Status { get; set; }
-​
-​
     }
 }
