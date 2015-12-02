8e648fc2cfda87016954ed47c94070cf43620c3f
John Knoll <jpknoll@ucdavis.edu>
2015-12-02 11:09:29 -0800

    placeholder

---
+++ src/Namster/Controllers/SearchController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.AspNet.Mvc;
+using Namster.Models;
+
+namespace Namster.Controllers
+{
+    public class SearchController : Controller
+    {
+        public DataNam[] Query(string term)
+        {
+            return new[]
+            {
+                new DataNam {NamNumber = "1234567"},
+                new DataNam {NamNumber = "1234567"},
+                new DataNam {NamNumber = "1234567"},
+            };
+        }
+    }
+}
