1e98883f039a66b17047713200bc1aac65a28323
John Knoll <jpknoll@ucdavis.edu>
2015-12-03 15:30:12 -0800

    placeholder with different nams

--- src/Namster/Controllers/SearchController.cs
+++ src/Namster/Controllers/SearchController.cs
@@ -6,13 +6,18 @@ namespace Namster.Controllers
 {
     public class SearchController : Controller
     {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
         public DataNam[] Query(string term)
         {
             return new[]
             {
                 new DataNam {NamNumber = "1234567"},
-                new DataNam {NamNumber = "1234567"},
-                new DataNam {NamNumber = "1234567"},
+                new DataNam {NamNumber = "1234568"},
+                new DataNam {NamNumber = "1234569"},
             };
         }
     }
