c581ceb0c953e28b6e965c0df967afa9728d2048
John Knoll <jpknoll@ucdavis.edu>
2015-12-04 11:17:13 -0800

    remove unneeded lib

--- src/Namster/gulpfile.js
+++ src/Namster/gulpfile.js
@@ -11,7 +11,6 @@ var gulp = require("gulp"),
     sass = require("gulp-sass"),
     cssmin = require("gulp-cssmin"),
     uglify = require("gulp-uglify"),
-    babel = require("gulp-babel"),
     webpack = require('webpack'),
     sourcemaps = require('gulp-sourcemaps');
 
