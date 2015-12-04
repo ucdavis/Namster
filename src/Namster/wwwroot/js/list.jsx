abd65c3cd51ffb19a26ab9631118ad4aefddabd5
Scott Kirkland <srkirkland@gmail.com>
2015-12-03 17:08:30 -0800

    basic example of react in action

--- src/Namster/wwwroot/js/list.jsx
+++ src/Namster/wwwroot/js/list.jsx
@@ -1,12 +1,39 @@
-﻿var HelloView = React.createClass({
+﻿var data = [{ id: 123, title: "abc" }, { id: 456, title: "def" }, { id: 789, title: "ghi" }];
+
+var Nam = React.createClass({
+    render: function () {
+        console.log(this.props);
+        return (
+            <li>testing: {this.props.data.id} -- {this.props.data.title}</li>
+        );
+    }
+});
+
+var NamList = React.createClass({
+    render: function () {
+        var namNodes = this.props.data.map(function(nam) {
+            return (
+                <Nam data={nam} />
+            );
+        });
+        return (
+            <ul>{namNodes}</ul>
+        );
+    }
+});
+
+var ListView = React.createClass({
     render: function() {
         return (
-            <h1>Hello, world!</h1>
+            <div>
+                <h1>Showing NAMs for CA&ES Dean's Office</h1>
+                <NamList data={this.props.data} />
+            </div>
         );
     }
 });
 
 ReactDOM.render(
-    <HelloView />,
+    <ListView data={data} />,
     document.getElementById('example')
 );
