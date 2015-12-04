3bd90ab6fcafb70e44cd623eb7c7536154b084c7
Scott Kirkland <srkirkland@gmail.com>
2015-12-03 16:37:10 -0800

    hello world jsx

---
+++ src/Namster/wwwroot/js/list.jsx
@@ -0,0 +1,12 @@
+﻿var HelloView = React.createClass({
+    render: function() {
+        return (
+            <h1>Hello, world!</h1>
+        );
+    }
+});
+
+ReactDOM.render(
+    <HelloView />,
+    document.getElementById('example')
+);
