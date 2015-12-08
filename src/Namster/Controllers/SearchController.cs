7f244e507d1ee6bf13fc0f772d0eb6e73968000d
John Knoll <jpknoll@ucdavis.edu>
2015-12-08 11:14:59 -0800

    actual search

--- src/Namster/Controllers/SearchController.cs
+++ src/Namster/Controllers/SearchController.cs
@@ -1,24 +1,31 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
 using Namster.Models;
+using Namster.Services;
 
 namespace Namster.Controllers
 {
     public class SearchController : Controller
     {
+        private readonly ISearchService _searchService;
+
+        public SearchController(ISearchService searchService)
+        {
+            _searchService = searchService;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
 
-        public DataNam[] Query(string term)
+        public async Task<IEnumerable<DataNam>> Query(string term)
         {
-            return new[]
-            {
-                new DataNam {NamNumber = "1234567"},
-                new DataNam {NamNumber = "1234568"},
-                new DataNam {NamNumber = "1234569"},
-            };
+            var results = await _searchService.FindByMatchAsync(term, 25);
+            return results.Hits.Select(h => h.Source);
         }
     }
 }
