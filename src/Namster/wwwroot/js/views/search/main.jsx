13758485ebbfe92beebca5d7958102e5282624cc
John Knoll <jpknoll@ucdavis.edu>
2015-12-09 15:16:21 -0800

    move set state

--- src/Namster/wwwroot/js/views/search/main.jsx
+++ src/Namster/wwwroot/js/views/search/main.jsx
@@ -21,6 +21,7 @@ export class SearchMain extends React.Component {
         this._searchTimer = 0;
 
         if (this.state.query){
+          this.state.searching = true;
           this._resetSearch();
         }
     }
@@ -37,6 +38,7 @@ export class SearchMain extends React.Component {
 
         // check if we should start a new one
         if (!!event.target.value && event.target.value.length > 2) {
+            this.setState({searching: true});
             this._resetSearch();
         }
     }
@@ -44,7 +46,6 @@ export class SearchMain extends React.Component {
     // delay start search by 500 ms
     _resetSearch() {
         clearTimeout (this._searchTimer);
-        this.setState({searching: true});
         this._searchTimer = setTimeout(this._startSearch.bind(this), 500);
     }
 
@@ -76,14 +77,18 @@ export class SearchMain extends React.Component {
 
         return (
           <div>
-              <div className="input-group" id="search-box">
-                <span className="input-group-btn">
-                  <button className="btn btn-default" type="button"><i className="fa fa-search"></i></button>
-                </span>
-                <input type="text" className="form-control" value={this.state.query} onChange={this.onChange.bind(this)} />
+            <div className="input-group" id="search-box">
+              <span className="input-group-btn">
+                <button className="btn btn-default" type="button"><i className="fa fa-search"></i></button>
+              </span>
+              <input type="text" className="form-control" value={this.state.query} onChange={this.onChange.bind(this)} />
+            </div>
+            <div className="results-container mdl-grid">
+              <div class="mdl-cell mdl-cell--2-col mdl-cell--hide-tablet mdl-cell--hide-phone"></div>
+              <div class="mdl-color--white mdl-shadow--4dp content mdl-color-text--grey-800 mdl-cell mdl-cell--8-col">
+                  {content}
               </div>
-            <hr/>
-            {content}
+            </div>
           </div>
         );
     }
