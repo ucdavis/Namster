9bf34794dc87bddbbcb543e0574d74d7800528c7
John Knoll <jpknoll@ucdavis.edu>
2015-12-15 14:36:43 -0800

    use material-ui

--- src/Namster/wwwroot/js/views/search/facets.jsx
+++ src/Namster/wwwroot/js/views/search/facets.jsx
@@ -1,60 +1,77 @@
-export class FacetItem extends React.Component {
-    componentDidMount() {
-        var el = ReactDOM.findDOMNode(this)
-        var checkboxes = el.getElementsByClassName("mdl-js-checkbox")
-        componentHandler.upgradeElements(checkboxes, "MaterialCheckbox");
+import React from 'react'
+
+import Checkbox from 'material-ui/lib/checkbox';
+import List from 'material-ui/lib/lists/list';
+import ListItem from 'material-ui/lib/lists/list-item';
+import Divider from 'material-ui/lib/divider';
+import { SelectableContainerEnhance } from 'material-ui/lib/hoc/selectable-enhance';
+
+var SelectableList = SelectableContainerEnhance(List);
+
+export class FacetListItem extends React.Component {
+
+  onCheck(event, checked) {
+    if (this.props.onCheck){
+      this.props.onCheck(this.props.value, checked)
     }
 
-    onChange(event) {
-        this.props.onChange(event.target.checked)
-    }
+    this.setState({switched: true})
+  }
 
-    render() {
-        return (
-            <li className="facet-item">
-              <label className="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect">
-                <input type="checkbox" className="mdl-checkbox__input" onChange={this.onChange.bind(this)} />
-                <span className="mdl-checkbox__label">{this.props.name} <span className="small">({this.props.count})</span></span>
-              </label>
-            </li>
-        )
-    }
+  render() {
+    return (
+      <ListItem value={this.props.value}
+        leftCheckbox={ <Checkbox value={this.props.value} onCheck={this.onCheck.bind(this)} /> }
+        primaryText={ <span>{this.props.value} <span className="small">({this.props.count})</span></span> }
+      />
+    )
+  }
 }
 
 export class FacetList extends React.Component {
-    onChange(category, key, value) {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            buildings: this.props.buildings || [],
+            departments: this.props.departments || [],
+            vlans: this.props.vlans || []
+        }
+    }
+
+    onCheck(category, key, value) {
         this.props.onChange(category, key, value);
+    }
+
+    handleUpdateSelectedIndex(e,index) {
     }
 
     render() {
         var self = this;
 
+        if (!this.props.facets) {
+            return (<div></div>)
+        }
+
         return (
             <div className="">
-                <h3>Buildings</h3>
-                <ul>
-                {this.props.facets.Building.Items.map(function(facet) {
-                   return (
-                      <FacetItem key={facet.Key} name={facet.Key} count={facet.DocCount} onChange={self.onChange.bind(self, "building", facet.Key)} />
-                   )
-                })}
-                </ul>
-                <h3>Department</h3>
-                <ul>
-                {this.props.facets.Department.Items.map(function(facet) {
-                   return (
-                      <FacetItem key={facet.Key} name={facet.Key} count={facet.DocCount} onChange={self.onChange.bind(self, "department", facet.Key)} />
-                   )
-                })}
-                </ul>
-                <h3>VLAN</h3>
-                <ul>
-                {this.props.facets.VLAN.Items.map(function(facet) {
-                   return (
-                      <FacetItem key={facet.Key} name={facet.Key} count={facet.DocCount} onChange={self.onChange.bind(self, "vlan", facet.Key)} />
-                   )
-                })}
-                </ul>
+                <SelectableList subheader="Building" valueLink={{value:this.state.buildings, requestChange:this.handleUpdateSelectedIndex.bind(this)}}>
+                    {this.props.facets.Building.Items.map(function(item) {
+                        return <FacetListItem key={item.Key} value={item.Key} count={item.DocCount} onCheck={self.onCheck.bind(self, "building")} />
+                    })}
+                </SelectableList>
+                <Divider />
+                <SelectableList subheader="Department" valueLink={{value:this.state.departments, requestChange:this.handleUpdateSelectedIndex.bind(this)}}>
+                    {this.props.facets.Department.Items.map(function(item) {
+                        return <FacetListItem key={item.Key} value={item.Key} count={item.DocCount} onCheck={self.onCheck.bind(self, "department")} />
+                    })}
+                </SelectableList>
+                <Divider />
+                <SelectableList subheader="VLAN" valueLink={{value:this.state.vlans, requestChange:this.handleUpdateSelectedIndex.bind(this)}}>
+                    {this.props.facets.VLAN.Items.map(function(item) {
+                        return <FacetListItem key={item.Key} value={item.Key} count={item.DocCount} onCheck={self.onCheck.bind(self, "vlan")} />
+                    })}
+                </SelectableList>
             </div>
         )
     }
