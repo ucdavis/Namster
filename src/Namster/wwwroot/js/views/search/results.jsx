de66f6d9b69cc66edfb8fb90942c17235b6ff444
John Knoll <jpknoll@ucdavis.edu>
2015-12-16 15:00:59 -0800

    add links

--- src/Namster/wwwroot/js/views/search/results.jsx
+++ src/Namster/wwwroot/js/views/search/results.jsx
@@ -11,26 +11,34 @@ export class SearchResult extends React.Component {
 
         var building = null;
         if (this.props.result.Building) {
-            building = <ListItem primaryText={this.props.result.Building} leftIcon={<i className="material-icons">account_balance</i>} innerDivStyle={innerDivStyle} />;
+            var buildingUrl = './list?building=' + encodeURIComponent(this.props.result.Building);
+            building = <ListItem href={buildingUrl} innerDivStyle={innerDivStyle}
+              primaryText={this.props.result.Building} leftIcon={<i className="material-icons">account_balance</i>} />;
         }
 
         var room = null;
         if (this.props.result.Room) {
-            room = <ListItem primaryText={this.props.result.Room} leftIcon={<i className="material-icons">hotel</i>} innerDivStyle={innerDivStyle} />;
+          var roomUrl = './list?building=' + encodeURIComponent(this.props.result.Building) + '&room=' + encodeURIComponent(this.props.result.Room);
+            room = <ListItem href={roomUrl} innerDivStyle={innerDivStyle}
+              primaryText={this.props.result.Room} leftIcon={<i className="material-icons">hotel</i>} />;
         }
 
         var department = null;
         if (this.props.result.Department) {
-            department = <ListItem primaryText={this.props.result.Department} leftIcon={<i className="material-icons">group</i>} innerDivStyle={innerDivStyle} />;
+          var departmentUrl = './list?department=' + encodeURIComponent(this.props.result.Department);
+            department = <ListItem href={departmentUrl} innerDivStyle={innerDivStyle}
+              primaryText={this.props.result.Department} leftIcon={<i className="material-icons">group</i>} />;
         }
 
         var vlan = null;
         if (this.props.result.Vlan) {
-            vlan = <ListItem primaryText={this.props.result.Vlan} leftIcon={<i className="material-icons">router</i>} innerDivStyle={innerDivStyle} />;
+          var vlanUrl = './list?vlan=' + encodeURIComponent(this.props.result.Vlan);
+            vlan = <ListItem href={vlanUrl} innerDivStyle={innerDivStyle}
+              primaryText={this.props.result.Vlan} leftIcon={<i className="material-icons">router</i>} />;
         }
 
         return (
-            <div className="result col-sm-4">
+            <div className="result col-sm-6">
               <div className="card">
                 <h4 className="card-header" style={{position: "relative"}}>
                   <i className="material-icons" style={{position: "absolute"}}>settings_input_hdmi</i>
