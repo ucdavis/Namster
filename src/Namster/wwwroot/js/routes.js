d23ec4ccb42c796985e6fa5d83a802eec4a373dc
John Knoll <jpknoll@ucdavis.edu>
2016-02-17 10:37:36 -0800

    index route and lists

--- src/Namster/wwwroot/js/routes.js
+++ src/Namster/wwwroot/js/routes.js
@@ -1,8 +1,11 @@
 import React from 'react'
-import { Route } from 'react-router'
+import { Route, IndexRoute } from 'react-router'
 import SearchMain from './containers/SearchMain'
+import List from './containers/List'
 
 export default (
-  <Route path="/"
-         component={SearchMain} />
+  <Route path="/">
+    <IndexRoute component={SearchMain} />
+    <Route path="list" component={List} />
+  </Route>
 )
