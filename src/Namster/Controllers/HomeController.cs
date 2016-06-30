1196d9896298672406cf84ca0fbb854089d36c6c
John Knoll <jpknoll@ucdavis.edu>
2016-06-30 15:25:05 -0700

    fix namespaces

--- src/Namster/Controllers/HomeController.cs
+++ src/Namster/Controllers/HomeController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNet.Authorization;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Namster.Controllers
