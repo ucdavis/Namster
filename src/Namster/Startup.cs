53b5918781d93507fa0f454751211a2b1d3ceae7
John Knoll <jpknoll@ucdavis.edu>
2016-06-30 15:29:52 -0700

    remove public setter

--- src/Namster/Startup.cs
+++ src/Namster/Startup.cs
@@ -29,7 +29,7 @@ namespace Namster
             Configuration = builder.Build();
         }
 
-        public IConfigurationRoot Configuration { get; set; }
+        public IConfigurationRoot Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
