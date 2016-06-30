683fab5062b19743dfc4dfd78955747d41e3951b
John Knoll <jpknoll@ucdavis.edu>
2016-06-30 14:18:17 -0700

    new namespace

--- src/Namster/Controllers/HomeController.cs
+++ src/Namster/Controllers/HomeController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Authorization;
-using Microsoft.AspNet.Mvc;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Namster.Controllers
 {
