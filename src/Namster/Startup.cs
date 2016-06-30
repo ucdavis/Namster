27f0d7b01cb8107e9339ffd5a5f5afc68b9d06bd
John Knoll <jpknoll@ucdavis.edu>
2016-06-30 15:27:36 -0700

    unused namespaces

--- src/Namster/Startup.cs
+++ src/Namster/Startup.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using CasAuthenticationMiddleware;
+﻿using CasAuthenticationMiddleware;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
@@ -10,8 +6,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using Namster.Models;
-using Namster.Services;
 
 namespace Namster
 {
