312c299475b75339c2e609eeef0e56436868550c
John Knoll <jpknoll@ucdavis.edu>
2016-07-01 16:34:31 -0700

    use new attributes

--- src/Namster/Models/DataNam.cs
+++ src/Namster/Models/DataNam.cs
@@ -1,45 +1,41 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Nest;
 
 namespace Namster.Models
 {
-    [ElasticType(Name = "datanam", IdProperty = "NamNumber")]
+    [ElasticsearchType(Name = "datanam", IdProperty = "NamNumber")]
     public class DataNam
     {
-        [ElasticProperty(Index = FieldIndexOption.NotAnalyzed)]
+        [String(Index = FieldIndexOption.NotAnalyzed)]
         public string NamNumber { get; set; }
 
-        [ElasticProperty(Index = FieldIndexOption.NotAnalyzed)]
+        [String(Index = FieldIndexOption.NotAnalyzed)]
         public string ExactBuilding => Building;
         public string Building { get; set; }
 
-        [ElasticProperty(Index = FieldIndexOption.NotAnalyzed)]
+        [String(Index = FieldIndexOption.NotAnalyzed)]
         public string ExactRoom => Room;
         public string Room { get; set; }
 
-        [ElasticProperty(Index = FieldIndexOption.NotAnalyzed)]
+        [String(Index = FieldIndexOption.NotAnalyzed)]
         public string ExactDepartment => Department;
+
         public string Department { get; set; }
 
-        [ElasticProperty(Index = FieldIndexOption.No, Store = true)]
+        [String(Index = FieldIndexOption.No, Store = true)]
         public string CaanZone { get; set; }
 
-        [ElasticProperty(Index = FieldIndexOption.No, Store = true)]
+        [String(Index = FieldIndexOption.No, Store = true)]
         public string BillingId { get; set; }
 
-        [ElasticProperty(Index = FieldIndexOption.NotAnalyzed)]
+        [String(Index = FieldIndexOption.NotAnalyzed)]
         public string Vlan { get; set; }
 
-        [ElasticProperty(Index = FieldIndexOption.No, Store = true)]
+        [String(Index = FieldIndexOption.No, Store = true)]
         public string DepartmentNumber { get; set; }
 
         public string Division { get; set; }
 
         public string Status { get; set; }
-
-
     }
 }
