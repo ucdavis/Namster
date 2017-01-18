e9bf432e1b2a4420fdf57766272f98327b84c8d4
John Knoll <jpknoll@gmail.com>
2017-01-18 12:47:07 -0800

    config for toolkit

--- src/Namster/webpack.config.js
+++ src/Namster/webpack.config.js
@@ -1,37 +1,50 @@
-var path = require('path');
-var webpack = require("webpack");
-
-var nodeModulesPath = path.resolve(__dirname, 'node_modules');
+const path = require('path');
+const webpack = require('webpack');
+const autoprefixer = require('autoprefixer');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-    entry: {
-        app: path.resolve(__dirname, "./wwwroot/js/app"),
-        vendor: ['react', 'react-dom', 'material-ui', 'redux', 'react-redux']
-    },
-    resolve: {
-        alias: {
-            'react': path.resolve(nodeModulesPath, 'react')
-        },
-        extensions: ['', '.jsx', '.js']
-    },
-
-    output: {
-        path: path.resolve("./wwwroot/js/dist/"),
-        filename: "[name].js"
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.jsx?$/,
-                loader: "babel-loader",
-                query: {
-                    presets: ["react", "es2015"]
-                },
-                exclude: [nodeModulesPath]
-            }
-        ]
-    },
-    plugins: [
-        new webpack.optimize.CommonsChunkPlugin(/* chunkName= */"vendor", /* filename= */"vendor.bundle.js"),
+  context: __dirname,
+  entry: {
+    app: path.resolve(__dirname, './wwwroot/js/app'),
+    vendor: ['react', 'react-dom', 'redux', 'react-redux', 'react-toolbox'],
+    client: ['webpack/hot/only-dev-server']
+  },
+  resolve: {
+    extensions: ['', '.jsx', '.js', '.css', '.scss', '.json'],
+    modulesDirectories: [
+      'node_modules',
+      path.resolve(__dirname, './node_modules')
     ]
+  },
+  output: {
+    path: path.resolve('./wwwroot/dist/'),
+    filename: '[name].js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        loaders: ['react-hot', 'babel']
+      }, {
+        test: /(\.scss|\.css)$/,
+        loader: ExtractTextPlugin.extract('style', 'css?sourceMap&modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss!sass')
+      }
+    ]
+  },
+  postcss: [autoprefixer],
+  sassLoader: {
+    data: '@import "theme/_config.scss";',
+    includePaths: [path.resolve(__dirname, './wwwroot')]
+  },
+  plugins: [
+    new ExtractTextPlugin('bundle.css', { allChunks: true }),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      filename: 'vendor.bundle.js',
+      minChunks: Infinity
+    })
+  ]
 };
