3f48948d77695a687e04f3a2f84634ca395fca9d
John Knoll <jpknoll@gmail.com>
2017-01-18 12:48:10 -0800

    linting

--- src/Namster/wwwroot/js/app.js
+++ src/Namster/wwwroot/js/app.js
@@ -1,17 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { Provider } from 'react-redux'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { Router, browserHistory } from 'react-router';
 
-import routes from './routes'
-import { Router, browserHistory } from 'react-router'
+import routes from './routes';
 
-import configureStore from './store/configureStore'
+import configureStore from './store/configureStore';
 
-const store = configureStore()
+const store = configureStore();
 
 ReactDOM.render(
   <Provider store={store}>
     <Router history={browserHistory} routes={routes} />
   </Provider>,
   document.getElementById('app')
-)
+);
