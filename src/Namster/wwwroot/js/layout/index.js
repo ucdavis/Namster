bdca0f641906b0da980715bd0c48f99e3d3a1e14
John Knoll <jpknoll@gmail.com>
2017-01-20 17:10:01 -0800

    new style file

--- src/Namster/wwwroot/js/layout/index.js
+++ src/Namster/wwwroot/js/layout/index.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import { Layout, NavDrawer, Panel, Sidebar } from 'react-toolbox';
 
+import styles from './index.scss';
+
 export default class AppLayout extends React.Component {
   render() {
     return (
-      <Layout>
-        <Panel>
-          {this.props.children}
-        </Panel>
-      </Layout>
+      <div>
+        <Layout className={styles.main}>
+          <Panel>
+            {this.props.children}
+          </Panel>
+        </Layout>
+      </div>
     );
   }
 }
