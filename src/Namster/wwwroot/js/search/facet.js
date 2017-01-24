2762061513fc97e0528970f002cf2663f410b14e
John Knoll <jpknoll@gmail.com>
2017-01-23 17:18:13 -0800

    collapse feature

--- src/Namster/wwwroot/js/search/facet.js
+++ src/Namster/wwwroot/js/search/facet.js
@@ -1,7 +1,18 @@
 import React from 'react';
 import Checkbox from 'react-toolbox/lib/checkbox';
+import ClassNames from 'classnames';
+
+import styles from './facet.scss';
 
 export default class Facet extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      collapsed: false
+    };
+  }
 
   _onCheck = (key, value) => {
     // if unchecking, clear selected key
@@ -12,14 +23,27 @@ export default class Facet extends React.Component {
     }
   }
 
+  _toggleCollapse = (event) => {
+    this.setState({
+      collapsed: !this.state.collapsed
+    });
+  }
+
   renderFacet(item) {
     const selected = (this.props.selectedKey === item.key);
+
     return (
       <Checkbox
         key={item.key}
         checked={selected}
-        label={item.key}
+        label={
+          <div className={styles.label}>
+            <div>{item.key}</div>
+            <div className={styles.labelCount}>({item.docCount})</div>
+          </div>
+        }
         onChange={(value) => this._onCheck(item.key, value)}
+        theme={{ text: styles.labelContainer }}
       />
     );
   }
@@ -28,9 +52,14 @@ export default class Facet extends React.Component {
     const { items } = this.props;
 
     return (
-      <div className="">
-        <h2>{this.props.subHeader}</h2>
-        {items.map((item) => this.renderFacet(item))}
+      <div className={ClassNames({ [styles.collapsed]: this.state.collapsed })}>
+        <div className={styles.headerContainer} onClick={this._toggleCollapse}>
+          <h2>{this.props.header}</h2>
+          <i className={ClassNames('material-icons', styles.collapseToggle)}>keyboard_arrow_up</i>
+        </div>
+        <div className={styles.facetContainer}>
+          {items.map((item) => this.renderFacet(item))}
+        </div>
       </div>
     );
   }
