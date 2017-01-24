d402f86fa499931adb8ae8179627b4d52546902a
John Knoll <jpknoll@gmail.com>
2017-01-23 17:18:33 -0800

    sample table

---
+++ src/Namster/wwwroot/js/search/table.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Table } from 'react-toolbox';
+
+const NamModel = {
+  namNumber: { type: Number },
+  building: { type: String },
+  room: { type: String },
+  vlan: { type: String },
+  department: { type: String },
+  departmentNumber: { type: String },
+  status: { type: String },
+  caanZone: { type: String },
+  billingId: { type: String },
+  division: { type: String }
+};
+
+export default class Facet extends React.Component {
+
+  render() {
+    return (
+      <Table
+        model={NamModel}
+        selectable
+        source={this.props.results}
+      />
+    );
+  }
+}
