d55f1f9a5ef8e0ff95d3c2fd43772ba92610f7b7
John Knoll <jpknoll@gmail.com>
2017-02-28 15:56:13 -0800

    set local names

--- src/Namster/webpack.config.js
+++ src/Namster/webpack.config.js
@@ -58,7 +58,8 @@ module.exports = {
             loader: 'css-loader',
             options: {
               modules: true,
-              importLoaders: 1
+              importLoaders: 1,
+              localIdentName: '[path][name]__[local]--[hash:base64:5]',
               sourceMap: true
             }
           }, {
