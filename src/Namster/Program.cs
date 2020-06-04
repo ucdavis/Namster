81b64d23b441bcd84100208c70d910deb10c6883
Spruce Weber-Milne <swebermilne@ucdavis.edu>
2020-06-04 16:36:49 -0700

    Allow debugging locally in environment "Production"

--- src/Namster/Program.cs
+++ src/Namster/Program.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 
 namespace Namster
 {
@@ -17,6 +20,21 @@ namespace Namster
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hostContext, config) =>
+                {
+                    // Extend default builder to allow loading user secrets when debugging locally in Production mode
+                    var env = hostContext.HostingEnvironment;
+
+                    if (!env.IsDevelopment() && string.Equals(Environment.GetEnvironmentVariable("ASPNETCORE_LOAD_SECRETS"), "True", StringComparison.OrdinalIgnoreCase))
+                    {
+                        var appAssembly = Assembly.Load(new AssemblyName(env.ApplicationName));
+
+                        if (appAssembly != null)
+                        {
+                            config.AddUserSecrets(appAssembly, optional: true);
+                        }
+                    }
+                })
                 .UseStartup<Startup>();
     }
 }
